47a8b6840628aff1d8a3467ec6ad5f706514938b
tanhkoi <tankhoi46@gmail.com>
2025-09-27 20:49:18 +0700

    refactor: init customer model

---
+++ src/main/java/org/example/ecommerceapi/model/Customer.java
@@ -0,0 +1,61 @@
+package org.example.ecommerceapi.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int customer_id;
+
+    @NotNull
+    @Size(max = 100)
+    @Column(name = "first_name", nullable = false, length = 100)
+    private String first_name;
+
+    @NotNull
+    @Size(max = 100)
+    @Column(name = "last_name", nullable = false, length = 100)
+    private String last_name;
+
+    @Email
+    @NotNull
+    @Size(max = 100)
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @NotNull
+    @Size(min = 8, max = 255)
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Size(max = 100)
+    @Column(name = "address", length = 100)
+    private String address;
+
+    @Size(max = 20)
+    @Pattern(regexp = "^\\+?[1-9]\\d{1,14}$")
+    @Column(name = "phone_number", length = 20)
+    private String phone_number;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant created_at;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private Instant updated_at;
+}
